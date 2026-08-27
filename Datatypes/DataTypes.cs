6df0c5e066fb3545f9185fe5ad233794534cfe35
Mohammad Frehat <mohammadfrehatx95@gmail.com>
2026-08-27 20:45:22 +0300

    Add files via upload

---
+++ Datatypes/DataTypes.cs
@@ -0,0 +1,19 @@
+using System;
+
+class DataTypes
+{
+    static void Main()
+    {
+        int age = 20;
+        string name = "Ahmad";
+        double salary = 1500.50;
+        bool isActive = true;
+        char grade = 'A';
+
+        Console.WriteLine(age);
+        Console.WriteLine(name);
+        Console.WriteLine(salary);
+        Console.WriteLine(isActive);
+        Console.WriteLine(grade);
+    }
+}

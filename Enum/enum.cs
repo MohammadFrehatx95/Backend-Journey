c74c600ffc99c81b80d1ef796008d1a6a3af35bf
Mohammad Frehat <mohammadfrehatx95@gmail.com>
2026-08-30 22:27:20 +0300

    Add employee status checking functionality
    Implemented IEmployeeCheckStatus interface and added EmployeeCheckStatus method to handle employee status checks.

--- Enum/enum.cs
+++ Enum/enum.cs
@@ -10,24 +10,53 @@ public class Program
 
         Console.WriteLine($"Name: {emp.Name}");
         Console.WriteLine($"Status: {emp.Status}");
-    }
 
+        emp.EmployeeCheckStatus();
+    }
 
     public enum EmployeeStatus
     {
         Active = 1,
         OnLeave = 2,
         Suspended = 3,
-        Terminated = 4,
+        Terminated = 4
     }
 
-    public class Employee
+    public class Employee : IEmployeeCheckStatus
     {
         public string Name { get; set; }
 
         public EmployeeStatus Status { get; set; }
+
+        public void EmployeeCheckStatus()
+        {
+            switch (Status)
+            {
+                case EmployeeStatus.Active:
+                    Console.WriteLine("Employee is active");
+                    break;
+
+                case EmployeeStatus.OnLeave:
+                    Console.WriteLine("Employee is on leave");
+                    break;
+
+                case EmployeeStatus.Suspended:
+                    Console.WriteLine("Employee is suspended");
+                    break;
+
+                case EmployeeStatus.Terminated:
+                    Console.WriteLine("Employee is terminated");
+                    break;
+
+                default:
+                    Console.WriteLine("Unknown employee status");
+                    break;
+            }
+        }
     }
-    
 
-
+    public interface IEmployeeCheckStatus
+    {
+        void EmployeeCheckStatus();
+    }
 }

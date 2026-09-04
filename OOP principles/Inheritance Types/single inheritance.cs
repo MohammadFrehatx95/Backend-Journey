d537859aadeca0544d56362c66ff505e36065040
Mohammad Frehat <mohammadfrehatx95@gmail.com>
2026-09-05 01:29:58 +0300

    Add single inheritance example with Vehicle and Car
    Implemented single inheritance with Vehicle and Car classes.

---
+++ OOP principles/Inheritance Types/single inheritance.cs
@@ -0,0 +1,30 @@
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Car car = new Car();
+
+        car.Brand = "BMW";
+
+        car.Start();
+        car.Drive();
+    }
+}
+
+public class Vehicle
+{
+    public string Brand { get; set; } = string.Empty;
+
+    public void Start()
+    {
+        Console.WriteLine($"{Brand} vehicle started.");
+    }
+}
+
+public class Car : Vehicle
+{
+    public void Drive()
+    {
+        Console.WriteLine($"{Brand} car is driving.");
+    }
+}
